14180df2f54c292a75e115f84b30c2ee2dcd5dba
Rick Tillery <github@tilleryweb.com>
2021-02-26 16:08:18 -0600

    Update fmtstertest.cpp

--- fmtstertest.cpp
+++ fmtstertest.cpp
@@ -1,4 +1,4 @@
-/* Copyright (c) 2021 Harman International.  All rights reserved.
+/* Copyright (c) 2021 Harman International Industries, Incorporated.  All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
